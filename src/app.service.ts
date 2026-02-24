df6ad9029ef72b6c75a120d0fb59cb34c1d1f8c6
John ⚒  Waw3ru <waweruj00@gmail.com>
2026-02-24 12:44:54 +0300

    chore: Added healthcheck route
    Signed-off-by: John ⚒  Waw3ru <waweruj00@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor() {}
+
+  healthcheck() {
+    return {
+      health: 'OK',
+      timestamp: new Date(),
+    };
   }
 }
